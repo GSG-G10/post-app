741ee10138102520b9e2780ed50c05d7520d6c33
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-08-17 13:25:05 +0300

    build the file structure for the project Relates #3

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = require('./app');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(app.get('port'), () => {
+  // eslint-disable-next-line no-console
+  console.log(`server is run at http://localhost:${app.get('port')}`);
+});
